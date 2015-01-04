a92dd864a027ea393fa957b9a7f7c7da4ef732dc
s22l1g11 <s22l1g11@gmail.com>
2015-01-04 03:20:34 +0100

    added source code

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>reCaptcha | dev.zimmerpforte.de</title>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+  </head>
+  <body>
+    <p>This is a test form.</p>
+    <form action="action.php" method="POST">
+      <label>Please insert a name:</label>
+      <input type="text" name="name" required="required" autofocus="autofocus" />
+      <div class="g-recaptcha" data-sitekey="6Lff-_8SAAAAAJ_e3HYvfMBaHBw-mkci_hT-KmsV"></div>
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>
