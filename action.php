d4fc705efc3d876b2fa54c2fcd0197cc1dfed441
s22l1g11 <s22l1g11@gmail.com>
2015-01-04 03:20:43 +0100

    added action.php

---
+++ action.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+    <title>reCaptcha | dev.zimmerpforte.de</title>
+  </head>
+  <body>
+  <?php
+    print_r($_POST);
+    echo '<br />';
+  ?>
+  </body>
+</html>
